fa6845b0cce462c919da14c76e27a6cab4a50713
David Royer <droyer01@gmail.com>
2018-08-28 08:05:24 -0400

    testing live version on netlify

--- src/plugins/wp.js
+++ src/plugins/wp.js
@@ -3,5 +3,5 @@ import WpApi from '~/lib/WpApi.js'
 export default ({ app }, inject) => {
   // Set `$wp` instance on `app`
   // This way we can use it in middleware and pages `asyncData`/`fetch`
-  app.$wp = new WpApi('http://admin.theartinmotion.com')
+  app.$wp = new WpApi('https://test1.wpapi.app')
 }
