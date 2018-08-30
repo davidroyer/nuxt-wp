3e49113e560587f73b8c7df3a3bff2a185dd9e5e
David Royer <droyer01@gmail.com>
2018-08-29 23:29:57 -0400

    update

--- src/plugins/wp.js
+++ src/plugins/wp.js
@@ -1,9 +1,8 @@
 import WpApi from '~/lib/WpApi.js'
-const wpApiUrl = 'https://test1.wpapi.app'
+const wpApiUrl = 'http://nuxt-wp.dev'
 
 export default ({ app }, inject) => {
   // Set `$wp` instance on `app`
   // This way we can use it in middleware and pages `asyncData`/`fetch`
   app.$wp = new WpApi(`${wpApiUrl}`)
 }
-
